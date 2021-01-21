9c6270664f62826965feb6c211d5050fa7287e81
Leandro Alves da Silva <leandro.dev@hotmail.com>
2021-01-21 11:48:41 -0300

    #02 - Criando classes context, user e userMap.

---
+++ AspNetCoreAngular.Data/Context/AspNetCoreAngularContext.cs
@@ -0,0 +1,26 @@
+﻿using AspNetCoreAngular.Data.Mappings;
+using AspNetCoreAngular.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreAngular.Data.Context
+{
+    public class AspNetCoreAngularContext : DbContext
+    {
+        public AspNetCoreAngularContext(DbContextOptions<AspNetCoreAngularContext> option)
+            : base(option) { }
+
+        #region "DBSets"
+        public DbSet<User> Users { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modeBuilder)
+        {
+            modeBuilder.ApplyConfiguration(new UserMap());
+
+            base.OnModelCreating(modeBuilder);
+        }    
+    }
+}
---
+++ AspNetCoreAngular.Data/Mappings/UserMap.cs
@@ -0,0 +1,18 @@
+﻿using AspNetCoreAngular.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreAngular.Data.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
